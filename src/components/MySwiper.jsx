860ea41b5ad7e6a092c7b068af0700b2e641d2dc
gustipardo <gusm2003@gmail.com>
2023-11-06 16:40:08 -0300

    Slider functional

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -1,13 +1,21 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
 
-import 'swiper/swiper.min.css'
-
-export default function MySwiper(name) {
+export default function ImageSlider({ props }) {
+    console.log(props)
   return (
-    <h1>{name}</h1>
-    // <Swiper>
-    //   <SwiperSlide>{name}</SwiperSlide>
-    //   <SwiperSlide>Slide 2</SwiperSlide>
-    // </Swiper>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {props.map((image, index) => (
+        <SwiperSlide key={index}>
+         <img src={`../../public/projects/Barrio_Walsh/${image}`} alt={`Image ${index + 1}`} />
+        </SwiperSlide>
+
+      ))}
+    </Swiper>
   );
 }
