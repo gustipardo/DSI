20796b380c50127bd49a1d6c335aea12fe78e8ee
gustipardo <gusm2003@gmail.com>
2023-11-16 12:25:34 -0300

    test

--- src/components/MySwiper.jsx
+++ src/components/MySwiper.jsx
@@ -2,8 +2,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 import 'swiper/css';
-export default function ImageSlider({ props }) {
-  console.log(props)
+export default function ImageSlider() {
   return (
     
     <Swiper
@@ -12,12 +11,10 @@ export default function ImageSlider({ props }) {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      {props.frontmatter.imagenes.map((image, index) => (
-        <SwiperSlide key={index}>
-         <img src={`../../public/${props.url}/${image}`} alt={`Image ${index + 1}`} />
-        </SwiperSlide>
-
-      ))}
+      <SwiperSlide><img src="../../public/1.jpg"/></SwiperSlide>
+      <SwiperSlide><img src="../../public/3.jpg"/></SwiperSlide>
+      <SwiperSlide><img src="../../public/2.png"/></SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
   );
 }
