5ad12b74d50fc5d55970cc33c55dda06fbeb11a2
gustipardo <gusm2003@gmail.com>
2023-11-27 11:13:13 -0300

    Test Title 3

---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,16 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+export default function Title({ posts}) {
+
+  const urlParts = window.location.href.split('/');
+
+  console.log(urlParts[urlParts.length-1])
+  name=urlParts[urlParts.length-1]
+console.log(posts)
+  const props = posts.find(prop => prop.frontmatter.name === name);
+    const Title = props.frontmatter.title
+  return (
+    <h1>{Title}</h1>
+  );
+}
