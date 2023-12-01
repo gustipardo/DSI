481b776e7455a3f4c5c020df68ad381f81f4beef
gustipardo <gusm2003@gmail.com>
2023-12-01 15:16:15 -0300

    Project Button remade on react

---
+++ src/components/ProjectButton.jsx
@@ -0,0 +1,29 @@
+import 'swiper/css';
+import '../styles/swiperComponent.css';
+
+export default function ImageSlider({ isRight, posts }) {
+  const projects = posts.map((post) => post.frontmatter.name);
+  const currentPath = window.location.href.split('/');
+  const currentProject = currentPath[currentPath.length - 1];
+
+  const index = projects.indexOf(
+    projects.find((project) => project === currentProject)
+  );
+  const Alenght = projects.length;
+
+  const newURL = isRight ? (index + 1) % Alenght : (index - 1 + Alenght) % Alenght;
+
+  return (
+    <a
+      className={`flex justify-center self-center ${
+        isRight ? 'right-arrow-class' : 'left-arrow-class'
+      }`}
+      href={projects[newURL]}
+    >
+      <img
+        src="/assets/arrow.png"
+        className={`h-12 ${isRight ? 'rotate-180' : ''}`}
+      />
+    </a>
+  );
+}
